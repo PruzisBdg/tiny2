77f33d02bf175ea5fadfad4432e6dba7fd5c43cd
pruzis57 <spjh1@wi.rr.com>
2019-02-21 07:46:44 -0600

    nothing

--- codeblocks_gcc/main.c
+++ codeblocks_gcc/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <conio.h>
 #include "lineedit.h"
+#include "ui_test_support.h"
 
 int main(void)
 {
    LineEdit_Init();
+   OStream_Reset();
 
    C8 ch = '\0';
    while( (ch = getch()) != 'q')
    {
-      printf("> %c\r\n", ch);
+      if( LineEdit_AddCh(ch) == 1)
+      {
+         LineEdit_AcceptLine();
+      }
+      //printf("> %c\r\n", ch);
    }
    printf("Tiny2 console build\r\n");
 }
